b2848eed831ebb0f40e064f0571a662d2c0918bf
P Gerard <paulfgerard@gmail.com>
2021-06-18 17:56:02 +0100

    Creation, with Swagger

---
+++ HashTool/Controllers/FileSubmit.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HashTool.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileSubmit : ControllerBase
+    {
+        private readonly ILogger<FileSubmit> _logger;
+
+        public FileSubmit(ILogger<FileSubmit> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] string sasToken)
+        {
+            if (string.IsNullOrWhiteSpace(sasToken))
+            {
+                _logger.LogError("Empty token received");
+                return BadRequest("SAS Token required");
+            }
+            return Ok();
+        }
+    }
+}
